af1ff53c5ea3ea7ff8a11be68ab776a68c8d9655
Mateusz Lewartowski <lewar2013c@gmail.com>
2020-09-09 14:29:14 +0200

    added header with style

---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Header.css';
+import logo from './ematpro.png';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+function Header() {
+  return (
+    <div className='header'>
+      <img className="header__logo" src={logo} alt="logo ematpro shop" />
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Premium</span>
+        </div>
+      </div>
+
+      <div className="header__optionBasket">
+        <ShoppingBasketIcon />
+        <span className="header__optionLineTwo header__basketCount">0</span>
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
