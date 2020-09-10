7e7c02d1d1e6e7e43509f085afcd54323a74e259
Mateusz Lewartowski <lewar2013c@gmail.com>
2020-09-10 12:22:16 +0200

    added signin page with style also added firebase config on my project

---
+++ src/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './Login.css';
+import logo from './ematpro.png';
+import { Link } from 'react-router-dom';
+
+function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = e => {
+    e.preventDefault();
+    ////firebase 
+  }
+
+  const register = e => {
+    e.preventDefault();
+    ////firebase 
+  }
+
+  return (
+    <div className='login'>
+      <Link to='/'>
+        <img className="login__logo" src={logo} alt="logo login page" />
+      </Link>
+
+      <div className="login__container">
+        <h1>Sign in</h1>
+        <form>
+
+          <h5>E-mail</h5>
+          <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+
+          <h5>Password</h5>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+
+          <button type='submit' onClick={signIn} className="login__signInButton">Sign in</button>
+
+        </form>
+        <p>By signing-in you agree eMATPRO Condition of Use & Sale. Please see our Privacy Notice, our Cookies Notice.</p>
+
+        <button onClick={register} className="login__registerButton">Create Account</button>
+      </div>
+    </div>
+  )
+}
+
+export default Login 
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYk7bhlm2RU7oq6ewT0N3GtZe8d5NbVCg",
+  authDomain: "ecommerceshopbarca.firebaseapp.com",
+  databaseURL: "https://ecommerceshopbarca.firebaseio.com",
+  projectId: "ecommerceshopbarca",
+  storageBucket: "ecommerceshopbarca.appspot.com",
+  messagingSenderId: "556631475379",
+  appId: "1:556631475379:web:a1538800667ece14755cc6",
+  measurementId: "G-F7Q07Z8DSE"
+};
